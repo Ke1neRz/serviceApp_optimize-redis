5a775a517c71a0b2e22f6c065e50513908bf9dde
Ke1neRz <eled.gist122@gmail.com>
2025-10-16 17:46:55 +1000

    make def get_price - no optimize

--- service/services/views.py
+++ service/services/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.db.models import Prefetch
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
@@ -8,6 +7,7 @@ from clients.models import Client
 
 class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
+        'plan',
         Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name',
                                                               'user__email'))
     ) # prefetch_related('client') - Возьмём всех клиентов разом сразу для всех подписок. (Тоесть теперь не будем брать для каждой подписки всех клиетов, а вытащим сразу)
