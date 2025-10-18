b0e21ad23ec1582a02e4f589d4ba0da4e99e9e0b
Ke1neRz <eled.gist122@gmail.com>
2025-10-18 15:20:46 +1000

    manual caching, receivers with redis and add parallel cellery

--- service/services/models.py
+++ service/services/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.core.validators import MaxValueValidator
+from django.db.models.signals import post_delete
 
 from clients.models import Client
-from services.tasks import set_price
+from services.receivers import delete_cache_total_sum
+from services.tasks import set_comment, set_price
 
 class Service(models.Model):
     name = models.CharField(max_length=50)
@@ -12,14 +14,15 @@ class Service(models.Model):
         return f"{self.name}"
     
     def __init__(self, *args, **kwargs):
-        super.__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.__full_price = self.full_price
     
     def save(self, *args, **kwargs):
         if self.full_price != self.__full_price:
             for subscription in self.subscriptions.all():
                 set_price.delay(subscription.id)
-        return super.save(*args, **kwargs)
+                set_comment.delay(subscription.id)
+        return super().save(*args, **kwargs)
 
 
 class Plan(models.Model):
@@ -37,20 +40,31 @@ class Plan(models.Model):
         return f"Tupe: {self.plan_type}"
     
     def __init__(self, *args, **kwargs):
-        super.__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.__discount_percent = self.discount_percent
     
     def save(self, *args, **kwargs):
         if self.discount_percent != self.__discount_percent:
             for subscription in self.subscriptions.all():
                 set_price.delay(subscription.id)
-        return super.save(*args, **kwargs)
+                set_comment.delay(subscription.id)
+        return super().save(*args, **kwargs)
 
 class Subscription(models.Model):
     client = models.ForeignKey(Client, on_delete=models.PROTECT, related_name='subscriptions')
     service = models.ForeignKey(Service, on_delete=models.PROTECT, related_name='subscriptions')
     plan = models.ForeignKey(Plan, on_delete=models.PROTECT, related_name='subscriptions') # у каждого поля с ForeignKey образуется id: plan -> plan_id
     price = models.PositiveIntegerField(default=0)
+    comment = models.CharField(max_length=50, default="", blank=True, null=True)
 
     def __str__(self):
         return f"Sub for {self.client}"
+    
+    def save(self, *args, **kwargs):
+        creating = not bool(self.id)
+        result =  super().save(*args, **kwargs)
+        if creating:
+            set_price.delay(self.id)
+        return result
+
+post_delete.connect(delete_cache_total_sum, sender=Subscription)
--- service/services/views.py
+++ service/services/views.py
@@ -1,6 +1,8 @@
 from django.db.models import Prefetch, Sum
+from django.core.cache import cache
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
+from django.conf import settings
 from services.serializers import SubscriptionSerializer
 from services.models import Subscription
 from clients.models import Client
@@ -20,8 +22,18 @@ class SubscriptionView(ReadOnlyModelViewSet):
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset()) # наш queryset который определяется выше
         response = super().list(request, *args, **kwargs)
+
+        price_cache = cache.get(settings.PRICE_CACHE_NAME)
+
+        if price_cache:
+            total_price = price_cache
+        else:
+            total_price = queryset.aggregate(total=Sum('price')).get('total')
+            cache.set(settings.PRICE_CACHE_NAME, total_price, 60 * 10)
+
         response_data = {'result': response.data}
-        response_data['total_amount'] = queryset.aggregate(total=Sum('price')).get('total') # get вернёт нам просто число
+        response_data['total_amount'] = total_price # get вернёт нам просто число
+        # response_data['total_amount'] = queryset.aggregate(total=Sum('price')).get('total') # get вернёт нам просто число
         response.data = response_data
         return response
     # Переопределение
